6d0532075b8577ed7e675a697a6d4918de6e683a
Esteban Garcia <estebangarcia2121@gmail.com>
2021-10-17 18:55:36 -0700

    Removed quotes

--- spawner.go
+++ spawner.go
@@ -40,7 +40,7 @@ func (s spawner) CreateCommand(cmd string, args string) (*exec.Cmd, error) {
 	if err != nil {
 		return nil, err
 	}
-	return exec.Command(spawnCmd, append(s[spawnCmd], " \""+args+"\"")...), nil
+	return exec.Command(spawnCmd, append(s[spawnCmd], args)...), nil
 }
 
 // getAvaiableSpawnCommand gets the first available spawn command. It returns
