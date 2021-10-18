c4777310987801beffc64cfe533d9be50f232482
Esteban Garcia <estebangarcia2121@gmail.com>
2021-10-18 10:04:56 -0700

    Added headers for subprocess output start and end

--- subprocess_test.go
+++ subprocess_test.go
@@ -1,8 +1,10 @@
 package subprocess_test
 
 import (
+	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/estebangarcia21/subprocess"
@@ -18,13 +20,24 @@ func TestSubprocess(t *testing.T) {
 	}
 
 	var opts []subprocess.Option
-	if val, _ := os.LookupEnv("SHOW_TEST_SUBPROCESS_OUTPUT"); val != "true" {
+	val, _ := os.LookupEnv("SHOW_TEST_SUBPROCESS_OUTPUT")
+
+	showSubprocessOutput := val == "true"
+	if !showSubprocessOutput {
 		opts = append(opts, subprocess.HideOutput)
 	}
 
 	sp := subprocess.New(opts...)
 
+	if showSubprocessOutput {
+		logTitle("Subprocess Output Begin")
+	}
+
 	err := sp.Start(cmdStr)
+
+	if showSubprocessOutput {
+		logTitle("Subprocess Output End")
+	}
 
 	if sp.ExitCode != 0 {
 		t.Fatalf("wanted exit code 0; got %d", sp.ExitCode)
@@ -33,4 +46,24 @@ func TestSubprocess(t *testing.T) {
 	if err != nil {
 		t.Fatalf("received error while executing subprocess: %v", err)
 	}
+}
+
+func logTitle(msg string) {
+	div := "========================================"
+	divLen := len(div)
+
+	// center the msg based on div length and print
+	msgLen := len(msg)
+	msgStart := (divLen - msgLen) / 2
+
+	var midStr string
+	for n := 0; n < msgStart; n++ {
+		midStr += " "
+	}
+
+	midStr += strings.ToUpper(msg)
+
+	fmt.Println(div)
+	fmt.Println(midStr)
+	fmt.Println(div)
 }
