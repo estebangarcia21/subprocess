c6062b9b58c4069ac7a47e07135a9a3dc0cf4e4d
Esteban Garcia <estebangarcia2121@gmail.com>
2021-10-24 14:02:51 -0700

    Used uniquely generated strings for output tests

--- subprocess_test.go
+++ subprocess_test.go
@@ -1,6 +1,8 @@
 package subprocess_test
 
 import (
+	"fmt"
+	"math/rand"
 	"runtime"
 	"strings"
 	"testing"
@@ -10,9 +12,9 @@ import (
 
 func TestExec(t *testing.T) {
 	crossPlatformTestMatrix{
-		"windows": subprocess.New("dir", subprocess.Silent),
-		"darwin":  subprocess.New("ls", subprocess.Arg("-lh"), subprocess.Silent),
-		"linux":   subprocess.New("ls", subprocess.Arg("-lh"), subprocess.Silent),
+		"windows": subprocess.New("dir", subprocess.HideStdout),
+		"darwin":  subprocess.New("ls", subprocess.Arg("-lh"), subprocess.HideStdout),
+		"linux":   subprocess.New("ls", subprocess.Arg("-lh"), subprocess.HideStdout),
 	}.Exec(func(s *subprocess.Subprocess) {
 		if err := s.Exec(); err != nil {
 			t.Fatalf("received error while executing subprocess: %v", err)
@@ -25,10 +27,11 @@ func TestExec(t *testing.T) {
 }
 
 func TestStdoutText(t *testing.T) {
+	u := randString(16)
 	crossPlatformTestMatrix{
-		"windows": subprocess.New("Write-Host Hello world!", subprocess.Silent),
-		"darwin":  subprocess.New("echo Hello world!", subprocess.Shell, subprocess.Silent),
-		"linux":   subprocess.New("echo Hello world!", subprocess.Shell, subprocess.Silent),
+		"windows": subprocess.New(fmt.Sprintf("Write-Host %s", u), subprocess.HideStdout),
+		"darwin":  subprocess.New(fmt.Sprintf("echo %s", u), subprocess.Shell, subprocess.HideStdout),
+		"linux":   subprocess.New(fmt.Sprintf("echo %s", u), subprocess.Shell, subprocess.HideStdout),
 	}.Exec(func(s *subprocess.Subprocess) {
 		if err := s.Exec(); err != nil {
 			t.Fatalf("received error while executing subprocess: %v", err)
@@ -36,17 +39,18 @@ func TestStdoutText(t *testing.T) {
 
 		stdout := s.StdoutText()
 
-		if i := strings.Index(stdout, "Hello world!"); i == -1 {
-			t.Fatal("expected to find \"Hello world!\" in the subprocess stdout")
+		if i := strings.Index(stdout, u); i == -1 {
+			t.Fatalf("expected to find \"%s\" in the subprocess stdout", u)
 		}
 	})
 }
 
 func TestStderrText(t *testing.T) {
+	u := randString(16)
 	crossPlatformTestMatrix{
-		"windows": subprocess.New("Write-Error \"Hello world!\"", subprocess.Silent),
-		"darwin":  subprocess.New(">&2 echo \"Hello world!\"", subprocess.Shell, subprocess.Silent),
-		"linux":   subprocess.New(">&2 echo \"Hello world!\"", subprocess.Shell, subprocess.Silent),
+		"windows": subprocess.New(fmt.Sprintf("Write-Error %s", u), subprocess.HideStderr),
+		"darwin":  subprocess.New(fmt.Sprintf(">&2 echo %s", u), subprocess.Shell, subprocess.HideStderr),
+		"linux":   subprocess.New(fmt.Sprintf(">&2 echo %s", u), subprocess.Shell, subprocess.HideStderr),
 	}.Exec(func(s *subprocess.Subprocess) {
 		if err := s.Exec(); err != nil {
 			t.Fatalf("received error while executing subprocess: %v", err)
@@ -54,8 +58,8 @@ func TestStderrText(t *testing.T) {
 
 		stderr := s.StderrText()
 
-		if i := strings.Index(stderr, "Hello world!"); i == -1 {
-			t.Fatal("expected to find \"Hello world!\" in the subprocess stdout")
+		if i := strings.Index(stderr, u); i == -1 {
+			t.Fatalf("expected to find \"%s\" in the subprocess stderr", u)
 		}
 	})
 }
@@ -68,5 +72,36 @@ func (c crossPlatformTestMatrix) Exec(test func(*subprocess.Subprocess)) {
 			test(s)
 			break
 		}
+	}
+}
+
+var randTokens = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = randTokens[rand.Intn(len(randTokens))]
+	}
+	return string(b)
+}
+
+func TestRandString(t *testing.T) {
+	strLen := 16
+
+	a := randString(strLen)
+	var b string
+
+	for n := 0; ; n++ {
+		if n > 99 {
+			t.Fatal("100 invocations of randString did not produce different results")
+		}
+		b = randString(strLen)
+		if a != b {
+			break
+		}
+	}
+
+	if len(a) != strLen && len(b) != strLen {
+		t.Fatalf("strings are not of specified length: %d; a=%d; b=%d", strLen, len(a), len(b))
 	}
 }
